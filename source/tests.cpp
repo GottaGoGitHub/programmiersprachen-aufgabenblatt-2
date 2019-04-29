191984b62a3ec092070ec78550af44331bc51ceb
GottaGoGitHub <ericgrunitz@gmail.com>
2019-04-29 17:09:44 +0200

    Add tests for Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,49 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+#include "vec2.cpp"
+
+
+Vec2 a; 
+Vec2 b{5.1f, -9.3f};
+
+TEST_CASE("test_for_init", "[vec2]"){
+  REQUIRE(a.x == 0.0f);
+  REQUIRE(a.y == 0.0f);
+  REQUIRE(5.1f == Approx(b.x));
+  REQUIRE(-9.3 == Approx(b.y));
+}
+
+
+TEST_CASE("test_for_plus", "[plus]"){
+  //init test vectors
+  Vec2 op_test_plus{1.3f, 4.0f};
+  Vec2 op_test_plus2{3.6f, 1.1f};
+  op_test_plus += op_test_plus2;
+  REQUIRE(op_test_plus.x == Approx(4.9f));
+  REQUIRE(op_test_plus.y == Approx(5.1f));
+}
+TEST_CASE("test_for_minus", "[minus]"){
+  Vec2 op_test_minus{4.0f , 3.0f};
+  Vec2 op_test_minus2{2.5f, 1.0f};
+  op_test_minus -= op_test_minus2;
+  REQUIRE(op_test_minus.x == Approx(1.5f));
+  REQUIRE(op_test_minus.y == Approx(2.0f));
+}
+TEST_CASE("test_for_mult", "[mult]"){
+  Vec2 op_test_mult{4.0f , 3.0f};
+  float s = 2.5f;
+  op_test_mult *= s;
+  REQUIRE(op_test_mult.x == Approx(10.0f));
+  REQUIRE(op_test_mult.y == Approx(7.5f));
+}
+TEST_CASE("test_for_div", "[div]"){
+  Vec2 op_test_div{4.0f , 3.0f};
+  float s = 2.0f;
+  op_test_div /= s;
+  REQUIRE(op_test_div.x == Approx(2.0f));
+  REQUIRE(op_test_div.y == Approx(1.5f));
+}
 
 int main(int argc, char *argv[])
 {
