b1aeab57ae5ea3751fd2e40fc8a089c1ae8094f4
GottaGoGitHub <ericgrunitz@gmail.com>
2019-05-05 21:32:43 +0200

    Adding color to Rectangle and Circle

--- source/circle.hpp
+++ source/circle.hpp
@@ -2,13 +2,14 @@
 #define CIRCLE_HPP
 #include <cmath>
 #include "vec2.hpp"
+#include "color.hpp"
 
 class Circle{
     public:
         Circle():
-        center_{1, 1}, radius_{1} {}
-        Circle(Vec2 const& a, float const& radius):
-        center_{a}, radius_{radius} {}
+        center_{1, 1}, radius_{1}, color_{0.5, 0.5, 0.5} {}
+        Circle(Vec2 const& a, float const& radius, rgb const& color):
+        center_{a}, radius_{radius}, color_{color} {}
     public:
         float circumference(Circle circ){
             float res = 2 * M_PI * circ.radius_;
@@ -17,6 +18,7 @@ class Circle{
     private:
         float radius_;
         Vec2 center_;
+        rgb color_;
 };
 
 #endif // CIRCLE_HPP
--- source/rectangle.hpp
+++ source/rectangle.hpp
@@ -1,14 +1,15 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
 #include "vec2.hpp"
+#include "color.hpp"
 
 class Rectangle{
     public: //constructor
         Rectangle(): //default
-        min_{1, 1}, max_{2, 2} {}
+        min_{1, 1}, max_{2, 2}, color_{0.8, 0.2, 0.3} {}
 
-        Rectangle(Vec2 const& a, Vec2 const& b): //member
-        min_{a}, max_{a + b} {}
+        Rectangle(Vec2 const& a, Vec2 const& b, rgb const& color): //member
+        min_{a}, max_{a + b}, color_{color} {}
 
     public: //methods
         float circumference(Rectangle rec){
@@ -20,6 +21,7 @@ class Rectangle{
     private:
         float length, height;
         Vec2 min_, max_, center;
+        rgb color_;
         
 };
 
